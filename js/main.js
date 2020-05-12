4e463fc7b3fa2b6919844144b8d53bd0503e1e7d
Blackstone94 <eroa94@gmail.com>
2020-05-11 23:21:57 -0500

    primeras variables

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 (function(){
     "use strict";
     document.addEventListener('DOMContentLoaded',function(){
-        console.log("listo");
+       // variable usuario
+       var nombre=document.getElementById("nombre");
+       var apellido=document.getElementById("apellido");
+       var email=document.getElementById("email");
+
+       //campos pases
+       var pase_dia=document.getElementById("pase_dia");
+       var pase_completo=document.getElementById("pase_completo");
+       var pase_dosDias=document.getElementById("pase_dosDias");
+
+       //botones y divs
+       var calcular=document.getElementById("calcular");
+       var errorDiv=document.getElementById("error");
+       var botonRegistro=document.getElementById("btnRegistro");
+       var resultado=document.getElementById("lista-productos");
+
     });//Dom cargado
 })();
