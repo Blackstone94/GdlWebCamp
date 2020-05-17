dcac07eac34f751abe57088329ece60ec5306458
Blackstone94 <eroa94@gmail.com>
2020-05-17 05:30:56 -0500

    estilos total

--- js/main.js
+++ js/main.js
@@ -15,7 +15,8 @@
        var calcular=document.getElementById("calcular");
        var errorDiv=document.getElementById("error");
        var botonRegistro=document.getElementById("btnRegistro");
-       var resultado=document.getElementById("lista-productos");
+       var lista_productos=document.getElementById("lista-productos");
+       var suma=document.getElementById("suma-total");
        
        //extras
        var camisas=document.getElementById('camisa_evento');
@@ -29,20 +30,41 @@
                 alert("Debes elegir un regalo");
                 regalo.focus();
             }else{
-                var boletosDia=pase_dia.value,
-                boletos2Dias=pase_dosDias.value,
-                boletosCompleto=pase_completo.value,
-                cantCamisas=camisas.value,
-                cantEtiquetas=etiquetas.value;
-                
-                console.log("boleto dia:"+boletosDia);
-                console.log("boletodos:"+boletos2Dias );
-                console.log("completo "+boletosCompleto)
+                var boletosDia=parseInt(pase_dia.value,10) || 0 ,
+                boletos2Dias=parseInt(pase_dosDias.value,10) || 0 ,
+                boletosCompleto=parseInt(pase_completo.value,10) || 0 ,
+                cantCamisas=parseInt(camisas.value,10) || 0 ,
+                cantEtiquetas=parseInt(etiquetas.value,10) || 0 ;
                 
                 var totalApagar=(boletosDia*30)+(boletos2Dias*45)+(boletosCompleto*50)+
                 ((cantCamisas*10)*.93)+(cantEtiquetas*10);
                 console.log("total "+totalApagar);
-            }
+
+                var listadoProductos=[];
+                if(boletosDia>0){
+                    listadoProductos.push(boletosDia+" Boletos por dia");
+                }
+                if(boletos2Dias>0){
+                    listadoProductos.push(boletos2Dias+" Boletos dos dias");
+                }
+                if(boletosCompleto>0){
+                    listadoProductos.push(boletosCompleto+" Boletos compeltos");
+                }
+                if(cantCamisas>0){
+                    listadoProductos.push(cantCamisas+" Camisas");
+                }
+                if(cantEtiquetas>0){
+                    listadoProductos.push(cantEtiquetas+" Etiquetas");
+                }
+                console.log(listadoProductos);
+                
+                lista_productos.innerHTML='';
+                lista_productos.style.display="block";
+                for(var i=0; i<listadoProductos.length;i++){
+                    lista_productos.innerHTML+=listadoProductos[i]+'<br>'
+                }
+                suma.innerHTML= "$ "+totalApagar.toFixed(2);
+            }//elegir regalo
 
        }
     });//Dom cargado
