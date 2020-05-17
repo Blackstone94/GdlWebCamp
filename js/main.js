152b163b081da63b59f34014de42176f2f55a4a8
Blackstone94 <eroa94@gmail.com>
2020-05-17 04:43:05 -0500

    avance js

--- js/main.js
+++ js/main.js
@@ -16,6 +16,34 @@
        var errorDiv=document.getElementById("error");
        var botonRegistro=document.getElementById("btnRegistro");
        var resultado=document.getElementById("lista-productos");
+       
+       //extras
+       var camisas=document.getElementById('camisa_evento');
+       var etiquetas=document.getElementById('etiquetas');
 
+       calcular.addEventListener('click',calcularMontos);
+       
+       function calcularMontos(event){
+            event.preventDefault();
+            if(regalo.value==''){
+                alert("Debes elegir un regalo");
+                regalo.focus();
+            }else{
+                var boletosDia=pase_dia.value,
+                boletos2Dias=pase_dosDias.value,
+                boletosCompleto=pase_completo.value,
+                cantCamisas=camisas.value,
+                cantEtiquetas=etiquetas.value;
+                
+                console.log("boleto dia:"+boletosDia);
+                console.log("boletodos:"+boletos2Dias );
+                console.log("completo "+boletosCompleto)
+                
+                var totalApagar=(boletosDia*30)+(boletos2Dias*45)+(boletosCompleto*50)+
+                ((cantCamisas*10)*.93)+(cantEtiquetas*10);
+                console.log("total "+totalApagar);
+            }
+
+       }
     });//Dom cargado
 })();
