f73248ba08b2f62f8736ec20871d467e9c6e993b
BlackStone94 <eroa94@gmail.com>
2020-10-12 08:39:24 -0500

    includes

--- registro.php
+++ registro.php
@@ -1,67 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="manifest" href="site.webmanifest">
-    <link rel="apple-touch-icon" href="icon.png">
-    <!-- Place favicon.ico in the root directory -->
-
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/all.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald|PT+Sans&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/main.css">
-
-    <meta name="theme-color" content="#fafafa">
-  </head>
-
-  <body>
-    <header class="site-header">
-      <div class="hero">
-        <div class="contenido-header">
-          <nav class="redes-sociales">
-              <a href="#"><i class="fab fa-facebook-f"></i></a>
-              <a href="#"><i class="fab fa-twitter"></i></a>
-              <a href="#"><i class="fab fa-pinterest-p"></i></a>
-              <a href="#"><i class="fab fa-youtube"></i></a>
-              <a href="#"><i class="fab fa-instagram"></i></a>
-          </nav>
-          <div class="informacion-evento">
-            <div class="clearfix">
-              <p class="fecha"><i class="far fa-calendar-alt"></i> 10-12 Mayo 2020</p>
-              <p class="ciudad"><i class="fas fa-map-marker-alt"></i> Guadalajara,MX</p>
-              <h1 class="nombre-sitio">GdlWebCamp</h1>
-              <p class="slogan">La mejor conferencia de <span>diseño web</span> </p>
-            </div>
-          </div><!--Informacion evento-->
-        </div>
-      </div><!--hero-->
-  </header><!--fin header-->
-  <div class="barra">
-    <div class="contenedor clearfix">
-      <div class="logo">
-        <img src="img/logo.svg" alt="Logo">
-      </div>
-      <div class="menu-movil">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <nav class="navegacion-principal clearfix">
-        <a href="#">Conferencia</a>
-        <a href="#">Calendario</a>
-        <a href="#">Invitados</a>
-        <a href="registro.html">Reservaciones</a>
-      </nav>
-    </div><!--contenedor-->
-  </div><!--Barra-->
+<?php include_once 'includes/templates/header.php'?>
 
   <section class="seccion contenedor">
-
     <h2>Registro de usuarios</h2>
     <form id="registro" class="registro" action="index.html" method="POST">
       <div id="datos_usuario" class="clearfix registro caja">
@@ -241,54 +180,7 @@
           </div><!--total-->
         </div><!--caja-->
     </div><!--resumen-->
-
-    
     </form>
   </section>
 
-
-  <footer class="site-foter">
-    <div class="contenedor clearfix">
-      <div class="footer-informacion">
-          <h3>Sobre <span>GdlWebCamp</span></h3>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime expedita eveniet at obcaecati dignissimos
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ducimus magni ex consectetur quasi,
-            cumque commodi eum rerum nostrum debitis suscipit alias aut, temporibus vel a harum tenetur ipsum quidem.</p>
-      </div>
-      <div class="ultimos-tweets">
-        <h3>Ultimos <span>Tweets</span></h3>
-        <ul>
-          <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet eligendi, adipisci ab eos accusantium at</li>
-          <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet eligendi, adipisci ab eos accusantium at</li>
-          <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet eligendi, adipisci ab eos accusantium at,</li>
-        </ul>
-      </div>
-      <div class="menu">
-        <h3>Redes <span>Sociales</span></h3>
-        <nav class="redes-sociales">
-          <a href="#"><i class="fab fa-facebook-f"></i></a>
-          <a href="#"><i class="fab fa-twitter"></i></a>
-          <a href="#"><i class="fab fa-pinterest-p"></i></a>
-          <a href="#"><i class="fab fa-youtube"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
-      </nav><!--fin navegacion footer-->
-      </div>
-    </div>
-    <p class="copyright">Todos los derechos reservados GdlWebCamp 2020 &copy;</p>
-  </footer>
-  </body>
-
-  <script src="js/vendor/modernizr-3.8.0.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
-  <script src="js/plugins.js"></script>
-  <script src="js/main.js"></script>
-
-  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-  <script>
-    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set','transport','beacon'); ga('send', 'pageview')
-  </script>
-  <script src="https://www.google-analytics.com/analytics.js" async></script>
-</body>
-</html>
+  <?php include_once 'includes/templates/footer.php'?>
