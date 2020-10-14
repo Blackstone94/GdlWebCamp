556816eb63e550d7dab85dd115fe8982ed16bbfa
BlackStone94 <eroa94@gmail.com>
2020-10-14 16:01:12 -0500

    332

--- calendario.php
+++ calendario.php
@@ -1,12 +1,11 @@
 <?php include_once 'includes/templates/header.php'?>
     <section class="seccion contenedor">
       <h2>Calendario de Eventos</h2>
-    </section><!--fin seccion-->
 
     <?php
         try{
           require_once('includes/funciones/bd_conexion.php');
-          $sql ="select evento_id,nombre_evento,fecha_evento,hora_evento,cat_evento,nombre_invitado,apellido_invitado ";
+          $sql ="select evento_id,nombre_evento,fecha_evento,hora_evento,cat_evento,icono,nombre_invitado,apellido_invitado ";
           $sql .="from eventos ";
           $sql .="inner join categoria_evento ";
           $sql .="on eventos.id_cat_evento=categoria_evento.id_categoria ";
@@ -21,7 +20,7 @@
 
     <div class="calendario">
         <?php
-        $calendario = array();//creacion de un array
+          $calendario = array();//creacion de un array
           while($eventos = $resultado->fetch_assoc()){
             $fecha=$eventos['fecha_evento'];
             $evento = array(
@@ -29,39 +28,44 @@
               'fecha'=>$eventos['fecha_evento'],
               'hora'=>$eventos['hora_evento'],
               'categoria'=>$eventos['cat_evento'],
-              'invitado'=>$eventos['nombre_invitado'].$eventos['apellido_invitado']
+              'icono'=>"fa ".$eventos['icono'],
+              'invitado'=>$eventos['nombre_invitado'].' '.$eventos['apellido_invitado']
             );
-            $calendario[$fecha][]=$evento;
-            ?>
-          <?php }?>
+          $calendario[$fecha][]=$evento;
+        }?>
 
-          <?php
-            foreach($calendario as $dia=>$lista_eventos){?>
-          <h3>
-              <i class= "fa fa-calendar"></i>
-              <?php
-              //Unix
-              setlocale (LC_TIME,'es_ES.UTF-8');
-              //Windows
-                setlocale(LC_TIME,'spanish');
-                echo utf8_encode( strftime("%A, %d de %B del %Y",strtotime($dia)));
-              ?>
-          </h3>
-          <?php foreach($lista_eventos as $evento){?>
-            <div class="dia">
-              <p class="titulo"><?php echo $evento['titulo']?></p>
-              <p class="hora">
-                  <i class="fa fa-clock-o" aria-hidden="true"></i>
-                  <?php echo $evento['fecha']." ". $evento['']?>
-              </p>
-            </div>
-            <?php }?>
-            <pre>
-              <?php var_dump($calendario);?>
-            </pre>
-    </div>
-    <?php
-      $conn->close();
-    ?>
+        <?php
+          foreach($calendario as $dia=>$lista_eventos){?>
+            <h3>
+                <i class= "fa fa-calendar"></i>
+                <?php
+                //Unix
+                setlocale (LC_TIME,'es_ES.UTF-8');
+                //Windows
+                  setlocale(LC_TIME,'spanish');
+                  echo utf8_encode( strftime("%A, %d de %B del %Y",strtotime($dia)));
+                ?>
+            </h3>
+            <?php foreach($lista_eventos as $evento){ ?>
+              <div class="dia">
+                <p class="titulo"><?php echo $evento['titulo']?></p>
+                <p class="hora">
+                    <i class="fa fa-clock" aria-hidden="true"></i>
+                    <?php echo $evento['fecha']." ". $evento['hora']?>
+                </p>
+                <p>
+                  <i class= "<?php echo $evento['icono']?>"></i>
+                  <?php echo $evento['categoria']?>
+                </p>
+                <p>
+                  <i class= "fa fa-user"></i>
+                  <?php echo $evento['invitado']?>
+                </p>
+              </div>
+            <?php }?><!--recorrer eventos-->
+        <?php } ?><!-- recorrer dias-->
+    </div><!-- div calendario-->
+    <?php $conn->close(); ?>
+  </section><!--fin seccion-->
+
 <?php include_once 'includes/templates/footer.php'?>
-
