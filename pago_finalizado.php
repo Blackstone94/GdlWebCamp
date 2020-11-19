d711006d0175ee666623aa8be2e9b6a0c098bdbd
BlackStone94 <eroa94@gmail.com>
2020-11-19 08:58:40 -0600

    436

--- pago_finalizado.php
+++ pago_finalizado.php
@@ -1,25 +1,29 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/5.0.0/normalize.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-    <link rel="stylesheet" href="css/estilos.css">
-  </head>
-  <body>
-      <div class="formulario">
-            <h2>Pagos con Paypal</h2>
-              <?php
-                $resultado=$_GET['exito'];
-                $paymentId =$_GET['paymentId'];
-                if($resultado==="true"){
-                    echo "El pago se realizo correctamente";
-                    echo "El ID es {$paymentId}";
-                }else{
-                    echo "El pago no se realizo";
-                }
-              ?>
-        </div>
-  </body>
-</html>
+<?php include_once 'includes/templates/header.php'?>
+  <section class="seccion contenedor">
+      <h2>Resumen Registro</h2>
+      <?php
+      $resultado=$_GET['exito'];
+      $paymentId=$_GET['paymentId'];
+      $id_pago=$_GET['id_pago'];
+      if($resultado=="true"){
+        echo "<div class='resultado correcto'>";
+        echo "El pago se realizó correctamente <br/>";
+        echo "el ID es {$paymentId}";
+        echo "</div>";
+
+        require_once ('includes/funciones/bd_conexion.php');
+        $stmt=$conn->prepare('UPDATE registrados SET pagado=?  WHERE ID_registrado=?');
+        $pagado=1;
+        $stmt->bind_param('ii',$pagado,$id_pago);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+      }else{
+        echo "<div class='resultado error'>";
+        echo "El pago no se realizo";
+        echo "</div>";
+      }
+      ?>
+  </section>
+
+<?php include_once 'includes/templates/footer.php'?>
