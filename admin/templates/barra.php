9dd1dba6a867736104cf8d9d327443e00058481f
BlackStone94 <eroa94@gmail.com>
2020-11-20 11:23:25 -0600

    441

--- admin/templates/barra.php
+++ admin/templates/barra.php
@@ -2,15 +2,13 @@
 <!-- Site wrapper -->
 <div class="wrapper">
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+  <nav class="main-header navbar navbar-expand navbar-blue navbar-light">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
     </ul>
-
-
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
@@ -32,4 +30,5 @@
       </li>
     </ul>
   </nav>
+
   <!-- /.navbar -->
