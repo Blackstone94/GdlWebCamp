fe6ed690072e807147545b8d5dba09c473cc1942
BlackStone94 <eroa94@gmail.com>
2020-11-23 11:03:33 -0600

    445

--- admin/insertar-admin.php
+++ admin/insertar-admin.php
@@ -1,5 +1,4 @@
 <?php
-  include_once('funciones/funciones.php');
   if(isset($_POST['agregar-admin'])){
     $usuario=$_POST['usuario'];
     $nombre=$_POST['nombre'];
@@ -11,6 +10,17 @@
     );
     //encriptar password
     $password_hashed=password_hash($password,PASSWORD_BCRYPT,$opciones);
-
+    try{
+      include_once('funciones/funciones.php');
+      $stmt=$conn->prepare("INSERT INTO admins(usuario,nombre,password) values(?,?,?)");
+      $stmt->bind_param("sss",$usuario,$nombre,$password_hashed);
+      $stmt->execute();
+      $stmt->close();
+      $conn->close();
+      
+      echo "ok";
+    }catch(Exception $e){
+      echo "Error ".$e.getMessage();
+    }
   }
 ?>
